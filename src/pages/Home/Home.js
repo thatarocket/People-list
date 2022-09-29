796d66b00c666f1fb577d366e88ab5b30d154846
Thaís de Souza <thais.de.s.rodrigues@accenture.com>
2022-09-29 09:06:02 -0300

    adding style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,14 @@
-import './Home.css';
 import React, { useState } from "react";
 import People from '../../components/People/People';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import './Home.css';
 
 function Home() {
     const[people,setPeople] = useState([]);
@@ -16,33 +24,47 @@ function Home() {
     }
     
     return (
-        <div>
-            <div className="home">
-                <input type="text" placeholder="nome" onChange={event => setName(event.target.value)}></input>
-                <input type="text" placeholder="idade" onChange={event => setAge(event.target.value)}></input>
-                <button onClick={addPerson}>Adicionar</button>  
-                
-                <table className='peopleClass'>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Idade</th>
-                            <th>Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            {
-                                people.map((person, index) => (
+        <div className="home">            
+            <div>
+                <h1 className='titleList'>Lista de pessoas</h1>
+                <TextField
+                    id="outlined-basic"
+                    className='initialInputs'
+                    label="Nome"
+                    type="text"
+                    onChange={event => setName(event.target.value)}
+                />
+                <TextField
+                    id="outlined-number"
+                    className='initialInputs'
+                    label="Idade"
+                    type="number"
+                    onChange={event => setAge(event.target.value)}
+                />
+                <Button className="btnAddPeople" variant="outlined" onClick={addPerson}>Adicionar</Button>  
+            </div>
+            <div className='people'> 
+                <Table className='peopleTable'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Nome</TableCell>
+                            <TableCell>Idade</TableCell>
+                            <TableCell>Ação</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            people.map((person, index) => (
                                     <People 
                                         key={index}
                                         name={person.name}
                                         age={person.age}
                                     />
                                 ))
-                            }
-                    </tbody>
-                </table>
-                       
+                        }
+                    </TableBody>
+                    
+                </Table>                     
             </div>
         </div>
     );
